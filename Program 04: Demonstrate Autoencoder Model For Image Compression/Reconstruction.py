e76834558630a90995ef449d74b7f9f1e397342b
randomplayer2002 <amith.hathwar02@gmail.com>
2023-10-31 18:04:58 +0530

    Done

---
+++ Program 04: Demonstrate Autoencoder Model For Image Compression/Reconstruction.py
@@ -0,0 +1,47 @@
+import numpy as np
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.datasets import mnist
+import matplotlib.pyplot as plt
+
+(x_train, _), (x_test, _) = mnist.load_data()
+# normalize all values between 0 and 1 and we will flatten the 28x28 images into vectors of size 784.
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+input_img = Input(shape=(784,))
+e1 = Dense(392,activation= 'relu')(input_img)
+ls = Dense(196,activation='relu')(e1)
+d1 = Dense(392,activation= 'relu')(ls)
+decoded = Dense(784,activation='sigmoid')(d1)
+
+autoencoder = Model(input_img, decoded)
+# this model maps an input to its encoded representation
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+
+autoencoder.fit(x_train, x_train,
+epochs=50,
+batch_size=256,
+shuffle=True,
+validation_data=(x_test, x_test))
+encoded_imgs = autoencoder.predict(x_test)
+decoded_imgs = encoded_imgs
+
+n = 10 # how many digits we will display
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+ 
+    # display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
