17ee81a508fc3d0e175c7fc8d393fb3923f977f3
randomplayer2002 <amith.hathwar02@gmail.com>
2023-12-12 16:39:18 +0530

    Done

---
+++ Program 07: 5.	Desing and implement a deep learning network for classification of textual documents/LSTM.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from keras.datasets import imdb
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Embedding
+from tensorflow.keras.preprocessing import sequence
+
+tf.random.set_seed(7)
+top_words = 6000
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words = top_words)
+max_review_length = 5
+x_train = sequence.pad_sequences(x_train,maxlen = max_review_length)
+x_test = sequence.pad_sequences(x_test,maxlen = max_review_length)
+
+print(x_train.shape)
+embedding_vecor_length = 32
+model = Sequential()
+model.add(Embedding(top_words,embedding_vecor_length,input_length=max_review_length))
+model.add(Dropout(0.2))
+model.add(LSTM(200))
+model.add(Dropout(0.2))
+model.add(Dense(1,activation='sigmoid'))
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+print(model.summary())
+model.fit(x_train,y_train,epochs=3,batch_size=64)
+scores = model.evaluate(x_test,y_test,verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
