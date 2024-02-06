6810ff2ed8c559a550c7fce558069d51eff6e968
randomplayer2002 <amith.hathwar02@gmail.com>
2024-02-06 17:15:12 +0530

    Done

--- Program 05: Design and implement a deep learning network for classification of textual documents/LSTM.py
+++ Program 05: Design and implement a deep learning network for classification of textual documents/LSTM.py
@@ -7,7 +7,7 @@ from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import Embedding
 from tensorflow.keras.preprocessing import sequence
 
-tf.random.set_seed(7)
+# tf.random.set_seed(7)
 top_words = 6000
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words = top_words)
 max_review_length = 5
