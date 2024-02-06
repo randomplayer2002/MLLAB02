8cc893cfd9ab529d2b7327f5ebc21cbb2d0ae46c
randomplayer2002 <amith.hathwar02@gmail.com>
2024-02-06 14:25:44 +0530

    Done

--- Program 03: Convolutional Neural Network(CNN) for Classification of Image Dataset/CNN.py
+++ Program 03: Convolutional Neural Network(CNN) for Classification of Image Dataset/CNN.py
@@ -18,7 +18,6 @@ print("x_train shape:", x_train.shape)
 print(x_train.shape[0], "train samples")
 print(x_test.shape[0], "test samples")
 
-
 # convert class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
@@ -37,14 +36,12 @@ model = keras.Sequential(
 )
 
 model.summary()
-
 batch_size = 128
 epochs = 15
 
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-
 model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
-
 score = model.evaluate(x_test, y_test, verbose=0)
+
 print("Test loss:", score[0])
 print("Test accuracy:", score[1])
