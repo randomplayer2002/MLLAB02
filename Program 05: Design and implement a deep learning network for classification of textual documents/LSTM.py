390213f9646963b7dfa49d1a70c8e8ca64a3c34e
randomplayer2002 <amith.hathwar02@gmail.com>
2024-02-07 05:37:14 +0530

    Done

--- Program 05: Design and implement a deep learning network for classification of textual documents/LSTM.py
+++ Program 05: Design and implement a deep learning network for classification of textual documents/LSTM.py
@@ -5,20 +5,24 @@ from tensorflow.keras.layers import Dense, LSTM, Dropout, Embedding
 from tensorflow.keras.preprocessing import sequence
 
 # tf.random.set_seed(7)
-(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words = 6000)
-x_train = sequence.pad_sequences(x_train,maxlen = 5)
-x_test = sequence.pad_sequences(x_test,maxlen = 5)
-print(x_train.shape)
+top_words = 6000
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words = top_words)
+max_review_length = 5
+x_train = sequence.pad_sequences(x_train,maxlen = max_review_length)
+x_test = sequence.pad_sequences(x_test,maxlen = max_review_length)
 
+print(x_train.shape)
+embedding_vector_length = 32
 model = Sequential()
-model.add(Embedding(top_words,embedding_vector_length,input_length=32))
+model.add(Embedding(top_words,embedding_vector_length,input_length=max_review_length))
 model.add(Dropout(0.2))
 model.add(LSTM(200))
-model.add(Dense(1,activation='sigmoid'))
-model.summary()
 
+model.summary()
+model.add(Dense(1,activation='sigmoid'))
 model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
 model.fit(x_train,y_train,epochs=3,batch_size=64)
 
 scores = model.evaluate(x_test,y_test,verbose=0)
+
 print("Accuracy: %.2f%%" % (scores[1]*100))
